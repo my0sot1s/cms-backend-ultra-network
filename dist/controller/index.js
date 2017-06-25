6797a6bde234e8105723c95a1f77ead793021206
Nguyễn Mạnh Tể <Te.Nguyen@MacBooks-MacBook-Pro.local>
2017-06-25 12:26:14 +0700

    auto commit

--- dist/controller/index.js
+++ dist/controller/index.js
@@ -3,4 +3,5 @@ require('./Stories').default,
 require('./StoryContent').default,
 require('./Posts').default,
 require('./Center').default,
-require('./Foods').default];
+require('./Foods').default,
+require('./Fb').default];
--- src/controller/index.js
+++ src/controller/index.js
@@ -4,5 +4,5 @@ export default [
   require('./Posts').default,
   require('./Center').default,
   require('./Foods').default,
-  // require('./Fb').default,
+  require('./Fb').default,
 ]
