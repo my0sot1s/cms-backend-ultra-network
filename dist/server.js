b7c74b4244262f0b97a518f37da6640694c3126c
Nguyễn Mạnh Tể <Te.Nguyen@MacBooks-MacBook-Pro.local>
2017-07-02 20:58:29 +0700

    auto commit

--- dist/server.js
+++ dist/server.js
@@ -8,33 +8,25 @@ var _Foods = require('./models/Foods');var _Foods2 = _interopRequireDefault(_Foo
 require('./models');
 var _controller = require('./controller');var _controller2 = _interopRequireDefault(_controller);
 var _index = require('./middleware/index');
-
-
 var _schema = require('./graphql/schema');var _schema2 = _interopRequireDefault(_schema);
 var _subscriptions = require('./graphql/subscriptions');
 var _graphql = require('graphql');
 var _subscriptionsTransportWs = require('subscriptions-transport-ws');
-var _graphqlServerExpress = require('graphql-server-express');function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function _toConsumableArray(arr) {if (Array.isArray(arr)) {for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {arr2[i] = arr[i];}return arr2;} else {return Array.from(arr);}} // import 'express-graphql'
-// import { corsOptions, headerConfig, setPubsubMiddleware } from './middleware'
+var _graphqlServerExpress = require('graphql-server-express');function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function _toConsumableArray(arr) {if (Array.isArray(arr)) {for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {arr2[i] = arr[i];}return arr2;} else {return Array.from(arr);}}
 // http://dev.apollodata.com/tools/graphql-subscriptions/setup.html#subscription-server
+
 console.log('Running...');
 var app = (0, _express2.default)();
 var PORT = process.env.PORT || 3001;
-
-//  ex https://medium.com/@simontucker/building-chatty-a-whatsapp-clone-with-react-native-and-apollo-part-1-setup-68a02f7e11
-
-
-// create server ws for graphql suubscrition
-
 var wsServe = (0, _http.createServer)(app);
-
-//Set our static file directory to public
+// Note:ex at https://medium.com/@simontucker/building-chatty-a-whatsapp-clone-with-react-native-and-apollo-part-1-setup-68a02f7e11
+// create server ws for graphql suubscrition
+// Set our static file directory to public
 app.use(_express2.default.static(_path2.default.join(__dirname, 'public')));
 // help express can read param with ?
 app.use(_bodyParser2.default.json());
 app.use(_bodyParser2.default.urlencoded({ extended: false }));
-// app.use(cors())
-app.use(require('cors')());
+app.use((0, _cors2.default)());
 //Allow CORS
 app.all('*', _index.middleware.header);
 // app.all('*', middleware.)
@@ -42,26 +34,28 @@ app.all('*', _index.middleware.header);
 app.get('/', function (req, res) {
   res.sendfile(_path2.default.join(__dirname, 'public/index.html'));
 });
-// app.get('/graphql', (req, res) => {
-//   res.send(`Opp can't support now!!!  ≧◉◡◉≦ `)
-// })
 
-// api send with controller
-
+// Note: Load all controllers is a array.
+// router uri: api/{router_name}
 var len = _controller2.default.length;
 for (var i = 0; i < len; i++) {
   app.use('/api', (0, _cors2.default)(_index.middleware.cors), _controller2.default[i]);
 }
 
+// Note: deploy map graphql to express
+// connect to !/graphiql in dev mode
 app.use.apply(app, ['/graphql'].concat(_toConsumableArray(_index.middleware.graphql), [(0, _graphqlServerExpress.graphqlExpress)({ schema: _schema2.default })]));
-
 app.use('/graphiql', (0, _graphqlServerExpress.graphiqlExpress)({
   endpointURL: '/graphql',
   subscriptionEnpoint:
-  process.env.NODE_ENV === 'development' ? 'ws://localhost:3001/subscriptions' : 'ws://https://baseserver.herokuapp.com/subscriptions' }));
+  process.env.NODE_ENV === 'development' ? 'ws://localhost:3001/subscriptions' : 'ws://baseserver.herokuapp.com/subscriptions' }));
 
 
 
+
+// Note: Deploy subscription server
+// work with websocket at: ws://localhost:3001/subscriptions of course work with dev model
+// subscripts to recieve broadcast
 _subscriptionsTransportWs.SubscriptionServer.create({
   execute: _graphql.execute,
   subscribe: _graphql.subscribe,
@@ -70,6 +64,9 @@ _subscriptionsTransportWs.SubscriptionServer.create({
   server: wsServe,
   path: '/subscriptions' });
 
+
+// Note: server using port 3001 in development
+// work with socket
 wsServe.listen(PORT, function () {
   console.log('*** started at ' + PORT + ' ***');
   console.log('+*******************************+');
--- src/server/server.js
+++ src/server/server.js
@@ -8,8 +8,6 @@ import Model from './models/Foods'
 import './models'
 import controller from './controller'
 import { middleware } from './middleware/index'
-// import 'express-graphql'
-// import { corsOptions, headerConfig, setPubsubMiddleware } from './middleware'
 import schema from './graphql/schema'
 import { pubsub } from './graphql/subscriptions'
 import { execute, subscribe } from 'graphql';
@@ -20,21 +18,15 @@ import { graphiqlExpress, graphqlExpress } from 'graphql-server-express'
 console.log(`Running...`)
 const app = express()
 let PORT = process.env.PORT || 3001
-
-//  ex https://medium.com/@simontucker/building-chatty-a-whatsapp-clone-with-react-native-and-apollo-part-1-setup-68a02f7e11
-
-
-// create server ws for graphql suubscrition
-
 const wsServe = createServer(app)
-
-//Set our static file directory to public
+// Note:ex at https://medium.com/@simontucker/building-chatty-a-whatsapp-clone-with-react-native-and-apollo-part-1-setup-68a02f7e11
+// create server ws for graphql suubscrition
+// Set our static file directory to public
 app.use(express.static(path.join(__dirname, 'public')));
 // help express can read param with ?
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cors())
-app.use(require('cors')())
+app.use(cors())
 //Allow CORS
 app.all('*', middleware.header);
 // app.all('*', middleware.)
@@ -42,26 +34,28 @@ app.all('*', middleware.header);
 app.get('/', (req, res) => {
   res.sendfile(path.join(__dirname, 'public/index.html'))
 })
-// app.get('/graphql', (req, res) => {
-//   res.send(`Opp can't support now!!!  ≧◉◡◉≦ `)
-// })
 
-// api send with controller
-
+// Note: Load all controllers is a array.
+// router uri: api/{router_name}
 const len = controller.length;
 for (var i = 0; i < len; i++) {
   app.use('/api', cors(middleware.cors), controller[i])
 }
 
+// Note: deploy map graphql to express
+// connect to !/graphiql in dev mode
 app.use('/graphql', ...middleware.graphql, graphqlExpress({ schema }));
-
 app.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql',
   subscriptionEnpoint:
   process.env.NODE_ENV === 'development'
     ? `ws://localhost:3001/subscriptions` :
-    `ws://https://baseserver.herokuapp.com/subscriptions`
+    `ws://baseserver.herokuapp.com/subscriptions`
 }))
+
+// Note: Deploy subscription server
+// work with websocket at: ws://localhost:3001/subscriptions of course work with dev model
+// subscripts to recieve broadcast
 SubscriptionServer.create({
   execute,
   subscribe,
@@ -70,6 +64,9 @@ SubscriptionServer.create({
     server: wsServe,
     path: '/subscriptions',
   });
+
+// Note: server using port 3001 in development
+// work with socket
 wsServe.listen(PORT, () => {
   console.log(`*** started at ${PORT} ***`)
   console.log(`+*******************************+`)
